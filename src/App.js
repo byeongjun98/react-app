ccc47583e787543adbe76e3ffe36479f1d10a413
ParkByeongJun <degea98@gmail.com>
2025-09-24 16:03:01 +0900

    [Feat] 컴포넌트 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function Header(props){
   console.log('props', props, props.title);
   return(
     <header>
-      <h1><a href="/">{props.title}</a></h1>
+      <h1><a href="/" onClick={(event)=>{
+        event.preventDefault()
+        props.onChangeMode()
+      }}>{props.title}</a></h1>
     </header>
   )
 }
@@ -12,7 +15,7 @@ function Header(props){
 function Article(props){
   return(
       <article>
-        <h2>{props.title}</h2>
+        <h2 >{props.title}</h2>
         {props.body}
       </article>
   )
@@ -22,7 +25,12 @@ function Nav(props){
   const lis = []
   for(let i = 0; i < props.topics.length; i++){
     let t = props.topics[i]
-    lis.push(<li key={t.id}><a href={"/read/" + t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id}href={"/read/" + t.id} onClick={event=>{
+        event.preventDefault()
+        props.onChangeMode(event.target.id)
+        }}>{t.title}</a>
+      </li>)
   }
   return(
       <nav>
@@ -33,7 +41,6 @@ function Nav(props){
   )
 }
 
-
 function App() {
   const topics = [
     {id: 1, title: 'html', body: 'html is ...'},
@@ -42,8 +49,12 @@ function App() {
   ]
   return (
     <div>
-      <Header title="React"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="React" onChangeMode={()=>{
+        alert('Header')
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id)
+      }}></Nav>
       <Article title="Welcome" body="Hello, Web!"></Article>
     </div>
   );
